f4978933d1da61f216e0301b5cc4255af8ebd93d
shivaprasadks <chethanshivu555@gmail.com>
2026-01-16 20:51:16 +0530

    feat: Initialize new Maven project for Playwright in Java with a basic example.

---
+++ src/main/java/org/derivedproduct1/com/Main.java
@@ -0,0 +1,24 @@
+package org.derivedproduct1.com;
+
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+        page.navigate("https://google.com");
+        System.out.println(page.title());
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot.png")));
+        browser.close();
+        playwright.close();
+    }
+}
